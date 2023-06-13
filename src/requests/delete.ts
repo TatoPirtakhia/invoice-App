a442395476e06e230fb5d65483e712b75be2f2dc
TatoPirtakhia <tatopirtakhia@gmail.com>
2023-06-13 22:33:49 +0400

    fix  request

--- src/requests/delete.ts
+++ src/requests/delete.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const deleteInvoice = async (id: string) => {
-  await axios.delete("https://invoice-app-rsqt.onrender.com/api/api/deleteInvoice/" + id)
+  await axios.delete("https://invoice-app-rsqt.onrender.com/api/deleteInvoice/" + id)
     .then((response) => {
       console.log(response);
     });
